dd039150189d31d1110298da048884950bf26e82
Louis Andrew <louisandrew3@gmail.com>
2021-01-16 18:07:34 +0100

    Finalizes login server-code.

--- backend/src/login/index.js
+++ backend/src/login/index.js
@@ -4,17 +4,30 @@
         ? alternative: POST /adviz/login -> with userID and password in the request body / header.
  */
 const router = require('express').Router() // initializing router object.
+const login = require('./login')
 const { debug } = require('./login')
 
 const defaultRoute = router.route('/')
 
-defaultRoute.get(async (req, res) => {
-    const useDebug = true // identifiier to use dbug.
+defaultRoute.post(async (req, res) => {
+    const { body } = req
+    const useDebug = false // set to true to always log in as admina. for development purposes.
 
-    const user = useDebug ? await debug() : null
+    const { userId, password } = body
 
+    // send error when no userId or password is provided
+    if (!userId || !password) {
+        res.status(401).send({ msg: 'Error' })
+        return
+    }
+
+    // just ignore the debug function.
+    const user = useDebug ? await debug() : await login(userId, password)
+
+    // check if user exists.
     if (await user) {
         await res.status(200).send({ user })
+        return
     }
 
     await res.status(401).send({ msg: 'Error' })
--- backend/src/login/login.js
+++ backend/src/login/login.js
@@ -5,23 +5,25 @@ const { User } = require('../models')
  * Always return the admina user.
  * @returns {(User | null)} returns null if no user is found and the User document if it is found.
  */
-const debug = async () => {
-    try {
-        const doc = await User.findOne({ userName: 'admina' })
-
-        return await doc
-    } catch (e) {
-        console.error(e)
-        return null
-    }
-}
+const debug = async () => login('admina', 'aaa')
 
 /**
  * Login functionality, checking if the user with username and password exists within the database
  * @param {String} username
  * @param {String} password
  */
-const login = (username, password) => {}
+const login = async (username, password) => {
+    try {
+        const doc = await User.findOne({
+            userName: username,
+            passWord: password,
+        }) // finds one document with the given username + password combo
+        return await doc
+    } catch (e) {
+        console.error(e)
+        return null
+    }
+}
 
 module.exports = login
 module.exports.debug = debug
