5209ca24a303f20e156e67fba332c43692463621
Louis Andrew <louisandrew3@gmail.com>
2021-01-30 10:07:46 +0100

    changing git ignore and appstate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
+import { useState } from 'react'
+
 import logo from './logo.svg'
 import './app.scss'
 
+/**
+ * Available states of the adviz app.
+ */
+const AppState = {
+    LOGIN: 1,
+    MAIN: 2,
+    // NEW: 3,
+    // UPDATE: 4,
+    LOADING: 99,
+}
+
+Object.freeze(AppState) // creating an enumeration
+// https://stackoverflow.com/questions/287903/what-is-the-preferred-syntax-for-defining-enums-in-javascript
+
+/**
+ * Main container of the whole Adviz app.
+ */
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [currUser, setCurrUser] = useState('') // username of the current loggedin user
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            {isLoggedIn ? 'Logged in' : 'I dont know u'}
+            <button
+                onClick={() => {
+                    setIsLoggedIn((prev) => !prev)
+                }}
+            >
+                Go to next state
+            </button>
         </div>
     )
 }
