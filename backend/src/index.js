97bcc57a950dbb91595bce7d1bf49e95a4c5361f
Louis Andrew <louisandrew3@gmail.com>
2021-02-03 11:42:15 +0100

    Moving dependencies.

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 // setup express server here
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(express.json()) // accepting JSON body
 app.use(cors())
